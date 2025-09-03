e82408e98ffa90d9815360711413ebb85116e925
Roman Zotov <hamomel@gmail.com>
2025-09-03 20:25:31 +0400

    conflict resolution added

--- app/src/main/java/com/hamomel/queens/game/ui/QueensGameViewModel.kt
+++ app/src/main/java/com/hamomel/queens/game/ui/QueensGameViewModel.kt
@@ -17,10 +17,60 @@ class QueensGameViewModel(
     fun onSquareClick(position: Position) {
         val pieceOnPosition = _viewState.value.board.getPiece(position)
         if (pieceOnPosition == null) {
-            mutateState {  it.board.setPiece(WhiteQueen, position) }
+            trySetQueen(position)
         } else {
             mutateState { it.board.removePiece(position) }
         }
+    }
+
+    private fun trySetQueen(position: Position) {
+        val conflicts = findConflicts(_viewState.value.board, position)
+
+        if (conflicts.isNotEmpty()) {
+            mutateState { it.copy(conflicts = conflicts) }
+        } else {
+            mutateState { it.board.setPiece(WhiteQueen, position) }
+        }
+    }
+
+    private fun findConflicts(board: Board, position: Position): List<Position> {
+        val conflicts = mutableListOf<Position>()
+
+        // Check row and column
+        for (i in 0 until board.size) {
+            val line = Position(position.line, i)
+            val column = Position(i, position.column)
+
+            if (i != position.column && board.getPiece(line) != null) {
+                conflicts.add(line)
+            }
+            if (i != position.line && board.getPiece(column) != null) {
+                conflicts.add(column)
+            }
+        }
+
+        // Check diagonals
+        for (i in -board.size until board.size) {
+            if (i != 0) {
+                val diagonal1 = Position(position.line + i, position.column + i)
+                val diagonal2 = Position(position.line + i, position.column - i)
+
+                if (diagonal1.line in 0 until board.size
+                    && diagonal1.column in 0 until board.size
+                    && board.getPiece(diagonal1) != null
+                ) {
+                    conflicts.add(diagonal1)
+                }
+                if (diagonal2.line in 0 until board.size
+                    && diagonal2.column in 0 until board.size
+                    && board.getPiece(diagonal2) != null
+                ) {
+                    conflicts.add(diagonal2)
+                }
+            }
+        }
+
+        return conflicts
     }
 
     private fun mutateState(mutation: (QueensGameViewState) -> Unit) {
--- app/src/main/java/com/hamomel/queens/game/ui/QueensGameViewState.kt
+++ app/src/main/java/com/hamomel/queens/game/ui/QueensGameViewState.kt
@@ -1,7 +1,9 @@
 package com.hamomel.queens.game.ui
 
 import com.hamomel.queens.data.Board
+import com.hamomel.queens.data.Position
 
 data class QueensGameViewState(
     val board: Board,
+    val conflicts: List<Position> = emptyList()
 )
